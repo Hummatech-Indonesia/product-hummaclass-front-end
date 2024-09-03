43d53c25a8acfc82ea8252dfff06c2c943c4170a
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-03 09:21:38 +0700

    membuat komponen modal hapus

---
+++ resources/views/components/delete-modal-component.blade.php
@@ -0,0 +1,25 @@
+<div class="modal fade" id="modal-delete" tabindex="-1" aria-labelledby="tambahdataLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <form id="form-delete" class="modal-content" method="post">
+            @csrf
+            @method('DELETE')
+            <div class="modal-header d-flex align-items-center" style="background-color: #7209DB;border-radius: 10px 10px 0 0;">
+                <h4 class="modal-title text-white" id="myModalLabel">
+                    Hapus data
+                </h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h6 class="fw-semibold text-dark">Apakah anda yakin akan menghapus data ini? </h6>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn text-white" style="background-color: #7209DB;" data-bs-dismiss="modal">
+                    Batal
+                </button>
+                <button type="submit" class="btn text-white" style="background-color: #DB0909;" data-bs-dismiss="modal">
+                    Hapus
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
