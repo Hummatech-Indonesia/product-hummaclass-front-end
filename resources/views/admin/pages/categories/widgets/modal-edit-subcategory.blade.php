ae670431223314b4c45d1fa5259768040b782104
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-04 11:52:02 +0700

    slicing modal edit subkategori

---
+++ resources/views/admin/pages/categories/widgets/modal-edit-subcategory.blade.php
@@ -0,0 +1,27 @@
+<div class="modal fade" id="modal-edit-subcategory" tabindex="-1" aria-labelledby="importPegawai" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content shadow-md">
+            <div class="modal-header" style="background-color: #7209DB;border-radius: 10px 10px 0 0;">
+                <h5 class="modal-title text-white" id="importPegawai">Edit Sub Kategori</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="" method="POST" enctype="multipart/form-data">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <div class="form-group">
+                            <label for="" class="mb-2 fw-semibold text-dark">Nama Sub Kategori</label>
+                            <input type="text" class="form-control" placeholder="Masukan nama sub kategori" name="name" value="{{ old('name') }}">
+                            @error('name')
+                                <span class="text-danger error-create">{{ $message }}</span>
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn text-white" style="background-color: #DB0909;" data-bs-dismiss="modal">Batal</button>
+                    <button type="submit" class="btn text-white" style="background-color: #7209DB;">Tambah</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
