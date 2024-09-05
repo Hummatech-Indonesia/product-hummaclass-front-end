de12b12d3a4552c3e6c4a21d5b39985d5a7dcd08
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-06 01:04:08 +0700

    tab deksripsi

---
+++ resources/views/admin/pages/courses/panes/tab-description.blade.php
@@ -0,0 +1,57 @@
+<div class="card position-relative">
+    <div class="badge badge-warning position-absolute text-dark p-2 shadow"
+        style="font-weight:bold;top: 10px; left: 10px;background-color:#FFC224;">Development
+    </div>
+    <div class="d-flex">
+        <img src="{{ asset('assets/img/courses/course_thumb01.jpg') }}" alt="" class="rounded">
+        <div class="align-items-start p-3">
+            <div class="align-items-center">
+                <img src="{{ asset('admin/dist/images/profile/user-1.jpg') }}" alt=""
+                    class="rounded-circle" width="34" height="34">
+                <span>David Millar</span>
+            </div>
+            <h4 style="font-weight: bold;" class="my-3">Learning Javascript With Javascript</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae fuga eligendi ratione at
+                accusamus repudiandae consequatur deleniti placeat possimus enim fugiat dolorem dolores
+                delectus, magni culpa alias voluptatem? Eveniet, blanditiis!</p>
+            <h5 style="font-weight: bold;" class="text-primary">Rp. 300.000 <span class="text-dark fw-normal"> /
+                    <span class="text-warning">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                            viewBox="0 0 24 24" fill="currentColor"
+                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path
+                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
+                        </svg>
+                    </span>
+                    (4.8 Reviews)</span></h5>
+            <div class="col-4">
+                <div
+                    class="btn d-flex btn-light-warning w-100 d-block text-warning font-medium mt-3 d-flex justify-content-start align-items-center">
+                    <span class="badge text-warning"><svg xmlns="http://www.w3.org/2000/svg" width="24"
+                            height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                            stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="icon icon-tabler icons-tabler-outline icon-tabler-book-2">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                            <path d="M19 4v16h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2h12z" />
+                            <path d="M19 16h-12a2 2 0 0 0 -2 2" />
+                            <path d="M9 8h6" />
+                        </svg><span class="text-dark" style="font-weight: bold;"> 8 Materi</span>
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="card">
+    <div class="p-3">
+        <h3 style="font-weight: bold;">Deskripsi</h3>
+        <hr>
+        <h4 style="font-weight: bold;">Course Description</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, assumenda voluptates?
+            Distinctio
+            rerum, corrupti ducimus, esse debitis illum nisi ratione natus accusamus suscipit sint quos commodi
+            iste
+            molestias alias voluptatem.</p>
+    </div>
+</div>
