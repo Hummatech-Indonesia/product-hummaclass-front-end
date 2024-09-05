bcd35e234c65f5a527767dfeaf760f63b0060d19
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-05 15:38:21 +0700

    membetulkan ikon di landing page

--- resources/views/user/pages/welcome.blade.php
+++ resources/views/user/pages/welcome.blade.php
@@ -10,7 +10,14 @@
                     <h3 class="title tg-svg" data-aos="fade-right" data-aos-delay="400">
                         Never Stop
                         <span class="position-relative">
-                            <span class="svg-icon" id="banner-svg" data-svg-icon="{{ asset('assets/img/objects/title_shape.svg') }}"></span>
+                            <span class="svg-icon" id="banner-svg">
+                                <!-- SVG langsung di sini -->
+                                <svg width="61" height="68" viewBox="0 0 61 68" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M10.9456 42.4604C12.35 35.8453 15.0985 20.3798 14.8574 11.4385" stroke="#031333" stroke-width="3.07158" stroke-linejoin="round"/>
+                                    <path d="M27.4487 52.5191C33.5478 49.598 47.4807 42.3448 54.4199 36.7009" stroke="#031333" stroke-width="3.07158" stroke-linejoin="round"/>
+                                    <path d="M20.1039 44.2553C23.1559 40.986 29.8591 33.2239 32.2559 28.3291" stroke="#031333" stroke-width="3.07158" stroke-linejoin="round"/>
+                                </svg>
+                            </span>
                             <svg x="0px" y="0px" preserveAspectRatio="none" viewBox="0 0 209 59" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M4.74438 7.70565C69.7006 -1.18799 136.097 -2.38304 203.934 4.1205C207.178 4.48495 209.422 7.14626 208.933 10.0534C206.793 23.6481 205.415 36.5704 204.801 48.8204C204.756 51.3291 202.246 53.5582 199.213 53.7955C136.093 59.7623 74.1922 60.5985 13.5091 56.3043C10.5653 56.0924 7.84371 53.7277 7.42158 51.0325C5.20725 38.2627 2.76333 25.6511 0.0898448 13.1978C-0.465589 10.5873 1.61173 8.1379 4.73327 7.70565" fill="currentcolor" />
                             </svg>
@@ -18,9 +25,10 @@
                         </span>
                         <br>Life <b>Never Stop</b> Teaching
                     </h3>
+                    
                     <p data-aos="fade-right" data-aos-delay="600">Every teaching and learning journey is unique Following We'll help guide your way.</p>
                     <div class="banner__btn-wrap" data-aos="fade-right" data-aos-delay="800">
-                        <a href="contact.html" class="btn arrow-btn">Start Free Trial <img src="assets/img/icons/right_arrow.svg" alt="img" class="injectable"></a>
+                        <a href="javascript:void(0)" class="btn arrow-btn">Uji Coba Gratis <img src="assets/img/icons/right_arrow.svg" alt="img" class="injectable"></a>
                     </div>
                 </div>
             </div>
