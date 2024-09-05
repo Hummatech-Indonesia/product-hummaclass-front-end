1caedc728c57bcc7a4cfe4fde42ea8aaaa0a7f4c
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-06 01:05:43 +0700

    slicing tab tugas akhir

---
+++ resources/views/admin/pages/courses/scripts/index.blade.php
@@ -0,0 +1,8 @@
+<script src="{{ asset('admin/dist/libs/prismjs/prism.js') }}"></script>
+<script src="{{ asset('admin/dist/libs/quill/dist/quill.min.js') }}"></script>
+
+<script>
+    var quill = new Quill("#editor", {
+      theme: "snow",
+    });
+  </script>
