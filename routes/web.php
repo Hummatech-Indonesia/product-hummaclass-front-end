2cdac44673ed4905b0064724b092496d44a19dd2
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-09 11:38:21 +0700

    route web

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminCourseController;
+use App\Http\Controllers\Admin\AdminSubModuleController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\ModuleController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\dashboard\StudentDashboardController;
+use App\Http\Controllers\Student\Profile\ProfileController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -22,5 +30,84 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-require_once __DIR__ . '/role/user.php';
-require_once __DIR__ . '/role/admin.php';
+// ================== USER ==================
+
+Route::prefix('dashboard')->name('dashboard.')->group(function () {
+    Route::resource('courses', CourseController::class)->only(['index', 'show']);
+    
+    Route::prefix('student')->name('student.')->group(function ()  {
+        Route::get('', [StudentDashboardController::class,'index'])->name('dashboard');
+        Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+        Route::get('courses', [CourseController::class, 'student'])->name('courses');
+    });
+});
+
+
+
+
+
+
+
+
+
+// ================== ADMIN ==================
+
+
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('home', function () {
+        return view('admin.index');
+    })->name('home');
+
+    Route::get('categories', function () {
+        return view('admin.pages.categories.index');
+    })->name('categories.index');
+
+    Route::get('module-questions', function () {
+        return view('admin.pages.courses.modules.module-questions.index');
+    });
+    Route::get('module-questions-create', function () {
+        return view('admin.pages.courses.modules.module-questions.create');
+    });
+
+    Route::resources([
+        'categories' => CategoryController::class,
+        'courses' => AdminCourseController::class,
+        'users' => UserController::class,
+        'modules' => ModuleController::class,
+        'sub-modules' => AdminSubModuleController::class
+    ]);
+
+
+    Route::get('courses/detail', function () {
+        return view('admin.pages.courses.detail');
+    });
+
+    Route::get('courses/detail/edit', function () {
+        return view('admin.pages.courses.edit');
+    });
+
+    Route::get('courses/detail-2', function () {
+        return view('admin.pages.courses.panes.moduls.detail-2');
+    });
+
+    Route::get('detail-users', function () {
+        return view('admin.pages.users.detail-users');
+    })->name('detail-users.index');
+
+    Route::get('create-modul/{course}', function() {
+        return view('admin.pages.courses.panes.moduls.create-modul');
+    })->name('create-moduls.index');
+
+    Route::get('create-materi', function() {
+        return view('admin.pages.courses.panes.moduls.create-materi');
+    })->name('create-materi.index');
+
+    Route::get('create-task', function() {
+        return view('admin.pages.courses.panes.moduls.create-task');
+    })->name('create-task.index');
+
+    Route::get('question-bank', function() {
+        return view('admin.pages.question-bank.index');
+    })->name('question-bank.index');
+});
