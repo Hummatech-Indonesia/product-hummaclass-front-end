5796b08cbf15b080c1b8e7bb510ec4a254773df5
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-09-12 11:05:50 +0700

    icon subcategori transisi admin

--- config/app.php
+++ config/app.php
@@ -58,7 +58,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    'api_url' => env('API_URL', "http://127.0.0.1:9000"),
+    'api_url' => env('API_URL', "http://127.0.0.1:8000"),
     'asset_url' => env('ASSET_URL'),
 
     /*
--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -1,122 +1,128 @@
 @extends('admin.layouts.app')
 
 @section('style')
-    <style>
-        .btn-close {
-            --bs-btn-close-bg: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/%3e%3c/svg%3e");
-            background: transparent var(--bs-btn-close-bg) center/1em auto no-repeat;
-        }
-    </style>
+<style>
+    .btn-close {
+        --bs-btn-close-bg: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/%3e%3c/svg%3e");
+        background: transparent var(--bs-btn-close-bg) center/1em auto no-repeat;
+    }
+
+    .icon {
+        transition: transform 0.3s ease;
+    }
+
+    .toggle-btn[aria-expanded="true"] .icon {
+        transform: rotate(180deg);
+    }
+
+</style>
 @endsection
 
 @section('content')
-    <div class="card position-relative overflow-hidden" style="background-color: #E8DEF3;">
-        <div class="card-body px-4 py-3">
-            <div class="row align-items-center">
-                <div class="col-9">
-                    <h5 class="fw-semibold mb-8">Kategori</h5>
-                    <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb">
-                            <li class="breadcrumb-item">
-                                <a class="text-muted " href="index-2.html">Daftar kategori</a>
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
-                <div class="col-3">
-                    <div class="text-center mb-n1">
-                        <img src="{{ asset('admin/dist/images/backgrounds/track-bg.png') }}" width="70px" alt=""
-                            class="img-fluid mb-n3" />
-                    </div>
+<div class="card position-relative overflow-hidden" style="background-color: #E8DEF3;">
+    <div class="card-body px-4 py-3">
+        <div class="row align-items-center">
+            <div class="col-9">
+                <h5 class="fw-semibold mb-8">Kategori</h5>
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <a class="text-muted " href="index-2.html">Daftar kategori</a>
+                        </li>
+                    </ol>
+                </nav>
+            </div>
+            <div class="col-3">
+                <div class="text-center mb-n1">
+                    <img src="{{ asset('admin/dist/images/backgrounds/track-bg.png') }}" width="70px" alt="" class="img-fluid mb-n3" />
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <div class="card p-3">
-        <h5 class="fw-semibold">Kategori</h5>
+<div class="card p-3">
+    <h5 class="fw-semibold">Kategori</h5>
 
-        <div class="d-flex justify-content-between mt-2">
-            <div action="" class="position-relative">
-                <input type="text" class="form-control product-search px-4 ps-5" name="name" value=""
-                    id="search-name" placeholder="Search">
-                <i class="ti ti-search position-absolute top-50 start-0 translate-middle-y fs-6 ms-3"
-                    style="color: #8B8B8B"></i>
-            </div>
-            <button class="btn text-white addCategory" style="background-color: #7209DB">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z" />
-                </svg>
-                Tambah Kategori
-            </button>
+    <div class="d-flex justify-content-between mt-2">
+        <div action="" class="position-relative">
+            <input type="text" class="form-control product-search px-4 ps-5" name="name" value="" id="search-name" placeholder="Search">
+            <i class="ti ti-search position-absolute top-50 start-0 translate-middle-y fs-6 ms-3" style="color: #8B8B8B"></i>
         </div>
-
-        <div class="table-responsive rounded-2 mb-4 mt-4">
-            <table class="table border text-nowrap customize-table mb-0 align-middle text-center">
-                <thead class="text-dark fs-4">
-                    <tr class="">
-                        <th class="fs-4 fw-semibold mb-0">No</th>
-                        <th class="fs-4 fw-semibold mb-0">Kategori</th>
-                        <th class="fs-4 fw-semibold mb-0">Sub Kategori</th>
-                        <th class="fs-4 fw-semibold mb-0">Tambah</th>
-                        <th class="fs-4 fw-semibold mb-0">Aksi</th>
-                    </tr>
-                </thead>
-                <tbody class="text-center" id="tableBody">
-                </tbody>
-            </table>
-        </div>
-        <div class="d-flex justify-content-end">
-            <nav id="pagination">
-
-            </nav>
-        </div>
+        <button class="btn text-white addCategory" style="background-color: #7209DB">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M19 12.998h-6v6h-2v-6H5v-2h6v-6h2v6h6z" />
+            </svg>
+            Tambah Kategori
+        </button>
     </div>
+
+    <div class="table-responsive rounded-2 mb-4 mt-4">
+        <table class="table border text-nowrap customize-table mb-0 align-middle text-center">
+            <thead class="text-dark fs-4">
+                <tr class="">
+                    <th class="fs-4 fw-semibold mb-0">No</th>
+                    <th class="fs-4 fw-semibold mb-0">Kategori</th>
+                    <th class="fs-4 fw-semibold mb-0">Sub Kategori</th>
+                    <th class="fs-4 fw-semibold mb-0">Tambah</th>
+                    <th class="fs-4 fw-semibold mb-0">Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="text-center" id="tableBody">
+            </tbody>
+        </table>
+    </div>
+    <div class="d-flex justify-content-end">
+        <nav id="pagination">
+
+        </nav>
+    </div>
+</div>
 @endsection
 @section('script')
-    <script>
-        let debounceTimer;
-        $('#search-name').keyup(function() {
-            clearTimeout(debounceTimer);
-            debounceTimer = setTimeout(function() {
-                get(1)
-            }, 500);
+<script>
+    let debounceTimer;
+    $('#search-name').keyup(function() {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(function() {
+            get(1)
+        }, 500);
+    });
+
+    function get(page) {
+        $('#tableBody').empty();
+        $.ajax({
+            type: "GET"
+            , url: "{{ config('app.api_url') }}" + "/api/categories?page=" + page
+            , dataType: "json"
+            , data: {
+                name: $('#search-name').val()
+            , }
+            , success: function(response) {
+                $.each(response.data.data, function(index, value) {
+                    $('#tableBody').append(category(index
+                        , value));
+                });
+
+                $('#pagination').html(handlePaginate(response.data.paginate))
+
+            }
+            , error: function(xhr) {
+                Swal.fire({
+                    title: "Terjadi Kesalahan!"
+                    , text: "Tidak dapat memuat data kategori."
+                    , icon: "error"
+                });
+            }
         });
+    }
 
-        function get(page) {
-            $('#tableBody').empty();
-            $.ajax({
-                type: "GET",
-                url: "{{ config('app.api_url') }}" + "/api/categories?page=" + page,
-                dataType: "json",
-                data: {
-                    name: $('#search-name').val(),
-                },
-                success: function(response) {
-                    $.each(response.data.data, function(index, value) {
-                        $('#tableBody').append(category(index,
-                            value));
-                    });
+    function category(index, value) {
+        let subCategoryRows = '';
 
-                    $('#pagination').html(handlePaginate(response.data.paginate))
-
-                },
-                error: function(xhr) {
-                    Swal.fire({
-                        title: "Terjadi Kesalahan!",
-                        text: "Tidak dapat memuat data kategori.",
-                        icon: "error"
-                    });
-                }
-            });
-        }
-
-        function category(index, value) {
-            let subCategoryRows = '';
-
-            if (value.sub_category.length > 0) {
-                $.each(value.sub_category, function(subIndex, subValue) {
-                    subCategoryRows += `
+        if (value.sub_category.length > 0) {
+            $.each(value.sub_category, function(subIndex, subValue) {
+                subCategoryRows += `
                         <tr class="sub_category">
                             <td style="background-color: #F6EEFE;"></td>
                             <td style="background-color: #F6EEFE;"></td>
@@ -140,16 +146,16 @@
                             </td>
                         </tr>
                     `;
-                });
-            } else {
-                subCategoryRows = `
+            });
+        } else {
+            subCategoryRows = `
                         <tr class="sub_category">
                             <td colspan="5">Tidak ada sub kategori</td>
                         </tr>
                     `;
-            }
+        }
 
-            return `
+        return `
                     <tr class="fw-semibold">
                         <td>${index + 1}</td>
                         <td>${value.name}</td>
@@ -157,8 +163,8 @@
                             <div class="d-flex justify-content-center">
                                 ${value.sub_category.length} Sub Kategori
                                 <p>
-                                    <button class="btn btn-sm text-white px-1 py-1 ms-2" type="button" style="background-color: #815FB4;" data-bs-toggle="collapse" data-bs-target="#collapseExample${index + 1}" aria-expanded="false" aria-controls="collapseExample${index + 1}">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
+                                    <button class="btn btn-sm text-white px-1 py-1 ms-2 toggle-btn" type="button" style="background-color: #815FB4;" data-bs-toggle="collapse" data-bs-target="#collapseExample${index + 1}" aria-expanded="false" aria-controls="collapseExample${index + 1}">
+                                        <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24">
                                             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2.0" d="m17 14l-5-5l-5 5"/>
                                         </svg>
                                     </button>
@@ -209,59 +215,60 @@
                         </td>
                     </tr>
                 `;
-        }
+    }
 
-        get(1);
+    get(1);
 
 
-        //delete catagory
-        $(document).on('click', '.btn-delete', function() {
-            const id = $(this).data('id');
-            const url = "{{ config('app.api_url') }}/api/categories/" + id;
+    //delete catagory
+    $(document).on('click', '.btn-delete', function() {
+        const id = $(this).data('id');
+        const url = "{{ config('app.api_url') }}/api/categories/" + id;
 
-            $('#modal-delete').modal('show');
-            deleteCategory(url);
+        $('#modal-delete').modal('show');
+        deleteCategory(url);
+    });
+
+    function deleteCategory(url) {
+        console.log(url);
+
+        $('.deleteConfirmation').click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: "DELETE"
+                , url: url
+                , dataType: "json"
+                , contentType: false
+                , processData: false
+                , success: function(response) {
+                    Swal.fire({
+                        title: "Berhasil!"
+                        , text: response.meta.message
+                        , icon: "success"
+                    });
+                    get(1)
+                }
+                , error: function(response) {
+                    Swal.fire({
+                        title: "Terjadi Kesalahan!"
+                        , text: "Ada kesalahan saat menyimpan data."
+                        , icon: "error"
+                    });
+                }
+            });
         });
 
-        function deleteCategory(url) {
-            console.log(url);
+    }
+    //end delete category
 
-            $('.deleteConfirmation').click(function(e) {
-                e.preventDefault();
-                $.ajax({
-                    type: "DELETE",
-                    url: url,
-                    dataType: "json",
-                    contentType: false,
-                    processData: false,
-                    success: function(response) {
-                        Swal.fire({
-                            title: "Berhasil!",
-                            text: response.meta.message,
-                            icon: "success"
-                        });
-                        get(1)
-                    },
-                    error: function(response) {
-                        Swal.fire({
-                            title: "Terjadi Kesalahan!",
-                            text: "Ada kesalahan saat menyimpan data.",
-                            icon: "error"
-                        });
-                    }
-                });
-            });
+</script>
 
-        }
-        //end delete category
-    </script>
+{{-- modal kategori --}}
+@include('admin.pages.categories.widgets.modal-edit')
+@include('admin.pages.categories.widgets.modal-create')
 
-    {{-- modal kategori --}}
-    @include('admin.pages.categories.widgets.modal-edit')
-    @include('admin.pages.categories.widgets.modal-create')
-
-    {{-- modal sub kategori --}}
-    @include('admin.pages.categories.widgets.modal-create-subcategory')
-    @include('admin.pages.categories.widgets.modal-edit-subcategory')
-    <x-delete-modal-component></x-delete-modal-component>
+{{-- modal sub kategori --}}
+@include('admin.pages.categories.widgets.modal-create-subcategory')
+@include('admin.pages.categories.widgets.modal-edit-subcategory')
+<x-delete-modal-component></x-delete-modal-component>
 @endsection
