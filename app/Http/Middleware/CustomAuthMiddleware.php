933818a60723fd4b586a6df84761549c26b2e498
Aryarizki8507 <mbedalitenga@gmail.com>
2024-09-23 15:06:50 +0700

    push

--- app/Http/Middleware/CustomAuthMiddleware.php
+++ app/Http/Middleware/CustomAuthMiddleware.php
@@ -17,8 +17,8 @@ class CustomAuthMiddleware
     public function handle(Request $request, Closure $next)
     {
         $token = session('hummaclass-token');
-        // dd($token);
-        $response = Http::withToken($token)->get(env('API_URL') . '/api/user');
+        // dd(config('app.api_url') . '/api/user');
+        $response = Http::withToken($token)->get(config('app.api_url') . '/api/user');
         if ($response->successful()) {
             return $next($request);
         }
