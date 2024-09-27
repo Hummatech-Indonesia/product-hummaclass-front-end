00eed9240114c0c67ad8709d95f8facdb58b8117
mohamadarif03 <mohamadarif5392@gmail.com>
2024-09-27 14:04:25 +0700

    Update edit-news.blade.php

--- resources/views/admin/pages/news/edit-news.blade.php
+++ resources/views/admin/pages/news/edit-news.blade.php
@@ -92,46 +92,52 @@
                     $('#thumbnail').attr('src', response.data.thumbnail);
                     $('#title').val(response.data.title);
                     $('#summernote-description').summernote('code', response.data.description);
-                    sub_category(response.data.category_id);
+                    sub_category(response.data.category_id, response.data.sub_category_id);
+                    category_id(response.data.category_id)
                 }
             });
 
+            function category_id(category_id = null) {
 
-            $.ajax({
-                type: "GET",
-                url: "{{ config('app.api_url') }}" + "/api/categories",
-                dataType: "json",
-                success: function(response) {
-                    $('#category_id').empty().append(
-                        '<option value="">Pilih Kategori</option>'
-                    );
-                    $.each(response.data.data, function(index, value) {
-                        $('#category_id').append(
-                            `<option value="${value.id}">${value.name}</option>`
+                $.ajax({
+                    type: "GET",
+                    url: "{{ config('app.api_url') }}" + "/api/categories",
+                    dataType: "json",
+                    success: function(response) {
+                        $('#category_id').empty().append(
+                            '<option value="">Pilih Kategori</option>'
                         );
-                    });
-                },
-                error: function(xhr) {
-                    Swal.fire({
-                        title: "Terjadi Kesalahan!",
-                        text: "Tidak dapat memuat data kategori.",
-                        icon: "error"
-                    });
-                }
-            });
+                        $.each(response.data.data, function(index, value) {
+                            $('#category_id').append(
+                                (category_id != null && category_id == value.id) ?
+                                `<option selected value="${value.id}">${value.name}</option>` :
+                                `<option value="${value.id}">${value.name}</option>`
+                            );
+                        });
+                    },
+                    error: function(xhr) {
+                        Swal.fire({
+                            title: "Terjadi Kesalahan!",
+                            text: "Tidak dapat memuat data kategori.",
+                            icon: "error"
+                        });
+                    }
+                });
+            }
 
             $('#category_id').on('change', function() {
                 var category_id = $(this).val();
                 sub_category(category_id);
             });
 
-            function sub_category(category_id) {
+            function sub_category(category_id, sub_category_id = null) {
                 if (!category_id) {
                     $('#sub_category_id').empty().append(
                         '<option value="">Pilih Sub Kategori</option>'
                     );
                     return;
                 }
+
 
                 $.ajax({
                     type: "GET",
@@ -144,6 +150,8 @@
 
                         $.each(response.data, function(index, value) {
                             $('#sub_category_id').append(
+                                (sub_category_id != null && sub_category_id == value.id) ?
+                                `<option selected value="${value.id}">${value.name}</option>` :
                                 `<option value="${value.id}">${value.name}</option>`
                             );
                         });
