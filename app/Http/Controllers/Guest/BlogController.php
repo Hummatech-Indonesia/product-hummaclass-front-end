6054e3ee9b8b5b7b0a8cd0712955c39c45c3653f
mohamadarif03 <mohamadarif5392@gmail.com>
2024-09-30 10:42:35 +0700

    push blog

--- app/Http/Controllers/Guest/BlogController.php
+++ app/Http/Controllers/Guest/BlogController.php
@@ -17,22 +17,6 @@ class BlogController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        // 
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show($id)
