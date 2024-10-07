512b5ad50447a90d7ffa53b62233e5612dacb4fb
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-10-07 10:35:57 +0700

    Update index.blade.php

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -194,156 +194,9 @@
             <div class="row">
                 <div class="col-lg-12 col-md-6 col-sm-12">
                     <div class="card">
-                        <div class="card-body">
+                        <div class="card-body" id="latest-review-section">
                             <h5 class="card-title fw-semibold">Review Terbaru</h5>
                             <p class="card-subtitle">Komentar Terbaru</p>
-                            <div class="mt-4 pb-3 border-bottom">
-                                <div class="d-flex align-items-center">
-                                    <span class="bg-light-primary text-primary badge">Development</span>
-                                    <span class="text-warning ms-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                    </span>
-                                    <span class="fs-3 ms-auto">8 March 2020</span>
-                                </div>
-                                <h6 class="mt-3">Arif Softcompound</h6>
-                                <span class="fs-3 lh-sm">Designing an NFT-themed website with a creative concept so
-                                    th...</span>
-                                <div class="hstack gap-3 mt-3">
-                                    <h6 class="fs-semibold">Learning JavaScript With Imagination...</h6>
-                                </div>
-                            </div>
-                            <div class="mt-4 pb-3 border-bottom">
-                                <div class="d-flex align-items-center">
-                                    <span class="bg-light-primary text-primary badge">Development</span>
-                                    <span class="text-warning ms-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                    </span>
-                                    <span class="fs-3 ms-auto">8 March 2020</span>
-                                </div>
-                                <h6 class="mt-3">Arif Softcompound</h6>
-                                <span class="fs-3 lh-sm">Designing an NFT-themed website with a creative concept so
-                                    th...</span>
-                                <div class="hstack gap-3 mt-3">
-                                    <h6 class="fs-semibold">Learning JavaScript With Imagination...</h6>
-                                </div>
-                            </div>
-                            <div class="mt-4 pb-3 border-bottom">
-                                <div class="d-flex align-items-center">
-                                    <span class="bg-light-primary text-primary badge">Development</span>
-                                    <span class="text-warning ms-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15"
-                                            height="15" viewBox="0 0 24 24" fill="currentColor"
-                                            class="icon icon-tabler icons-tabler-filled icon-tabler-star">
-                                            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                            <path
-                                                d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
-                                        </svg>
-                                    </span>
-                                    <span class="fs-3 ms-auto">8 March 2020</span>
-                                </div>
-                                <h6 class="mt-3">Arif Softcompound</h6>
-                                <span class="fs-3 lh-sm">Designing an NFT-themed website with a creative concept so
-                                    th...</span>
-                                <div class="hstack gap-3 mt-3">
-                                    <h6 class="fs-semibold">Learning JavaScript With Imagination...</h6>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -471,6 +324,57 @@
                         </div>
                         `)
                     });
+                }
+            });
+
+            $.ajax({
+                type: "get",
+                url: "{{ config('app.api_url') }}/api/course-reviews-latest",
+                dataType: "json",
+                success: function(response) {
+                    let reviews = '';
+                    let latestReviewSection = $('#latest-review-section');
+                    response.data.forEach(review => {
+                        // Mengonversi created_at menjadi format tanggal yang diinginkan
+                        let date = new Date(review.created_at);
+                        let formattedDate = date.toLocaleDateString('id-ID', {
+                            day: 'numeric',
+                            month: 'short',
+                            year: 'numeric'
+                        });
+
+                        let stars = '';
+                        // Menambahkan bintang sesuai dengan nilai rating
+                        for (let i = 0; i < review.rating; i++) {
+                            stars += `
+                                <span class="text-warning ms-1">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="mb-1" width="15" height="15" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-star">
+                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                        <path d="M8.243 7.34l-6.38 .925l-.113 .023a1 1 0 0 0 -.44 1.684l4.622 4.499l-1.09 6.355l-.013 .11a1 1 0 0 0 1.464 .944l5.706 -3l5.693 3l.1 .046a1 1 0 0 0 1.352 -1.1l-1.091 -6.355l4.624 -4.5l.078 -.085a1 1 0 0 0 -.633 -1.62l-6.38 -.926l-2.852 -5.78a1 1 0 0 0 -1.794 0l-2.853 5.78z" />
+                                    </svg>
+                                </span>
+                            `;
+                        }
+
+                        reviews += `
+                        <div class="mt-4 pb-3 border-bottom">
+                            <div class="d-flex align-items-center">
+                                <span class="bg-light-primary text-primary badge">${review.course.sub_category.name}</span>
+                                <div class="d-flex align-items-center ms-2">
+                                    ${stars}
+                                </div>
+                                <span class="fs-3 ms-auto">${formattedDate}</span>
+                            </div>
+                            <h6 class="mt-3">${review.user.name}</h6>
+                            <span class="fs-3 lh-sm">Designing an NFT-themed website with a creative concept so th...</span>
+                            <div class="hstack gap-3 mt-3">
+                                <h6 class="fs-semibold">${review.course.title}</h6>
+                            </div>
+                        </div>
+                        `;
+                    });
+
+                    latestReviewSection.append(reviews);
                 }
             });
 
