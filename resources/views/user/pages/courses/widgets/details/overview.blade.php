6644479f1ec445b39caec8e3f0fd18facfcbcf47
mohamadarif03 <mohamadarif5392@gmail.com>
2024-10-08 08:18:15 +0700

    push

--- resources/views/user/pages/courses/widgets/details/overview.blade.php
+++ resources/views/user/pages/courses/widgets/details/overview.blade.php
@@ -1,29 +1,7 @@
 <div class="tab-pane fade show active" id="overview-tab-pane" role="tabpanel" aria-labelledby="overview-tab" tabindex="0">
     <div class="courses__overview-wrap">
-        <h3 class="title" id="description-title">Course Description</h3>
+        <h3 class="title" id="description-title">Kursus Deskripsi</h3>
         <p id="description-description"></p>
-        {{-- <ul class="about__info-list list-wrap">
-            <li class="about__info-list-item">
-                <i class="flaticon-angle-right"></i>
-                <p class="content">Work with color & Gradients & Grids</p>
-            </li>
-            <li class="about__info-list-item">
-                <i class="flaticon-angle-right"></i>
-                <p class="content">All the useful shortcuts</p>
-            </li>
-            <li class="about__info-list-item">
-                <i class="flaticon-angle-right"></i>
-                <p class="content">Be able to create Flyers, Brochures, Advertisements</p>
-            </li>
-            <li class="about__info-list-item">
-                <i class="flaticon-angle-right"></i>
-                <p class="content">How to work with Images & Text</p>
-            </li>
-        </ul>
-        <p class="last-info">Morem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua Quis ipsum suspendisse
-            ultrices gravida. Risus commodo viverra maecenas accumsan.Dorem ipsum dolor sit
-            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magn.</p> --}}
+
     </div>
 </div>
