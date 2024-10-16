06656d879c63490c62875bf843d6c7904b116f35
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-10-16 08:37:50 +0700

    slicing tab task

---
+++ resources/views/user/pages/courses/widgets/details/task.blade.php
@@ -0,0 +1,60 @@
+<div class="tab-pane fade" id="task-tab-pane" role="tabpanel" aria-labelledby="task-tab" tabindex="0">
+    <div class="card p-4 border-0" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);">
+        <div class="tab-pane fade active show" id="tab-pending" role="tabpanel" aria-labelledby="pending-tab" tabindex="0">
+            <div class="table-responsive">
+                <table id="demo-foo-addrow" class="table m-t-30 text-start table-hover contact-list footable footable-5 footable-paging footable-paging-center breakpoint-lg" data-paging="true" data-paging-size="7" style="">
+                    <thead>
+                        <tr class="footable-header">
+                            <th class="text-white" style="display: table-cell;background-color:#9425FE;">Tugas</th>
+                            <th class="text-white" style="display: table-cell;background-color:#9425FE;">Dikumpulkan pada</th>
+                            <th class="text-white" style="display: table-cell;background-color:#9425FE;">Status</th>
+                            <th class="text-white" style="display: table-cell;background-color:#9425FE;">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="pending-content">
+                        <tr>
+                            <td>Lorem ipsum dolor sit amet, consectetur..</td>
+                            <td>10 Januari 2024</td>
+                            <td>
+                                <span class="badge text-success" style="background-color: #EEFEF0;">Selesai</span>
+                            </td>
+                            <td>
+                                <button class="outline-purple-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="15" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M3 13c3.6-8 14.4-8 18 0"/><path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6"/></g></svg>
+                                    Detail
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Lorem ipsum dolor sit amet, consectetur..</td>
+                            <td>10 Januari 2024</td>
+                            <td>
+                                <span class="badge text-danger" style="background-color: #FEEEEE;">Revisi</span>
+                            </td>
+                            <td>
+                                <button class="outline-purple-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="15" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M3 13c3.6-8 14.4-8 18 0"/><path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6"/></g></svg>
+                                    Detail
+                                </button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Lorem ipsum dolor sit amet, consectetur..</td>
+                            <td>10 Januari 2024</td>
+                            <td>
+                                <span class="badge" style="background-color: #F6EEFE;color:#9425FE;">Proses</span>
+                            </td>
+                            <td>
+                                <button class="outline-purple-primary">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="17" height="15" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path d="M3 13c3.6-8 14.4-8 18 0"/><path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6"/></g></svg>
+                                    Detail
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
+</div>
