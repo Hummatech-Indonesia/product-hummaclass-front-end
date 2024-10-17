1307da58b9dac6beac4f17400b0346ec1baf3d98
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-10-17 08:23:12 +0700

    riwayat quiz user

--- resources/views/user/pages/courses/courses-lesson.blade.php
+++ resources/views/user/pages/courses/courses-lesson.blade.php
@@ -111,10 +111,41 @@
                                         <li>Waktu tunggu ujian ulang ; 1 menit</li>
                                     </ul>
                                     <div class="text-end mt-3 mb-4">
-                                        <a href="{{ route('quetion-quiz.index') }}" class="btn">Mulai Ujian</a>
+                                        <a href="" class="btn">Mulai Ujian</a>
+                                    </div>
+                                    <div class="table-responsive mt-3">
+                                        <table id="demo-foo-addrow"
+                                            class="table table-striped table-bordered m-t-30 text-center table-hover contact-list footable footable-5 footable-paging footable-paging-center breakpoint-lg"
+                                            data-paging="true" data-paging-size="7" style="">
+                                            <thead>
+                                                <tr class="footable-header">
+                                                    <th class="text-white"
+                                                        style="display: table-cell;background-color:#9425FE;">
+                                                        Tanggal</th>
+                                                    <th class="text-white"
+                                                        style="display: table-cell;background-color:#9425FE;">
+                                                        Nilai</th>
+                                                    <th class="text-white"
+                                                        style="display: table-cell;background-color:#9425FE;">
+                                                        Status</th>
+                                                    <th class="text-white"
+                                                        style="display: table-cell;background-color:#9425FE;">
+                                                        Action</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody id="user-quizzes">
+                                                {{-- <tr>
+                                                    <td>8 Des 2024 07:30</td>
+                                                    <td>100</td>
+                                                    <td><span class="bg-success p-2 text-white rounded text-center">Lulus</span></td>
+                                                    <td><button class="btn btn-primary">Lihat Detail</button></td>
+                                                </tr> --}}
+                                            </tbody>
+                                        </table>
                                     </div>
                                 </div>
                             </div>
+
 
                             <div class="footer__bottom" style="background-color: #FFFFFF;">
                                 <div class="container">
@@ -222,6 +253,28 @@
 
 @section('script')
     <script>
+        $.ajax({
+            type: "GET",
+            url: "{{ config('app.api_url') }}" + "/api/user-quizzes",
+            headers: {
+                Authorization: 'Bearer ' + "{{ session('hummaclass-token') }}"
+            },
+            dataType: "json",
+            success: function(response) {
+                $.each(response.data, function(index, value) {
+                    $('#user-quizzes').append(
+                        `
+                            <tr>
+                                <td>${value.created}</td>
+                                <td>${value.score}</td>
+                                <td><span class="bg-success p-2 text-white rounded text-center">${value.status}</span></td>
+                                <td><a class="btn btn-primary" href="">Lihat Detail</a></td>
+                            </tr> 
+                            `
+                    );
+                });
+            }
+        });
         // Definisikan fungsi di scope global
         function updateLastStepUser(course, sub_module) {
             $.ajax({
