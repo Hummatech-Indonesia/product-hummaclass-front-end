6ddf306634f6cda9239972dabfa41eb99c78cf1b
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-10-29 00:32:16 +0700

    slicing halaman detail tugas tab tugas

--- resources/views/admin/pages/courses/panes/moduls/detail-task.blade.php
+++ resources/views/admin/pages/courses/panes/moduls/detail-task.blade.php
@@ -6,7 +6,9 @@
         background-color: transparent;
         color: #000000;
     }
-
+    .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active {
+        background-color: #9425FE;
+    }
 </style>
 @endsection
 
@@ -33,33 +35,59 @@
     </div>
 </div>
 
-<div class="card">
-    <div class="p-4">
-        <div class="d-flex align-items-center gap-2">
-            <div class="d-flex justify-content-center align-items-center" style="background-color: #F6EEFE;border-radius:50%;width:30px;height:30px;">
-                <svg xmlns="http://www.w3.org/2000/svg" style="color: var(--purple-primary);" width="20" height="20" viewBox="-2 -2 24 24">
-                    <path fill="currentColor" d="M6 0h8a6 6 0 0 1 6 6v8a6 6 0 0 1-6 6H6a6 6 0 0 1-6-6V6a6 6 0 0 1 6-6m0 2a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V6a4 4 0 0 0-4-4zm6 7h3a1 1 0 0 1 0 2h-3a1 1 0 0 1 0-2m-2 4h5a1 1 0 0 1 0 2h-5a1 1 0 0 1 0-2m0-8h5a1 1 0 0 1 0 2h-5a1 1 0 1 1 0-2m-4.172 5.243L7.95 8.12a1 1 0 1 1 1.414 1.415l-2.828 2.828a1 1 0 0 1-1.415 0L3.707 10.95a1 1 0 0 1 1.414-1.414z" /></svg>
+<div class="card p-3 mt-3">
+    <div>
+        <ul class="nav nav-tabs d-flex justify-content-between" role="tablist">
+            <div class="d-flex">
+                <li class="nav-item detailTask">
+                    <a class="nav-link active" data-bs-toggle="tab" href="#detailTask" role="tab">
+                        <span>Tugas</span>
+                    </a>
+                </li>
+                <li class="nav-item listCollect">
+                    <a class="nav-link" data-bs-toggle="tab" href="#listCollect" role="tab">
+                        <span>Mengumpulkan</span>
+                    </a>
+                </li>
             </div>
-            <h6 class="mt-1">Tugas 1</h6>
-        </div>
-        <h3 id="title" class="fw-semibold mt-3">Membuat CRUD Blala</h3>
-    </div>
-    <hr>
-    <div class="p-4">
-        <h3>Deskripsi</h3>
-        <p id="description"></p>
-        <hr>
-        {{-- <h4>Unorder list.</h4>
-        <ul style="list-style-type: disc;" class="ms-4 text-black">
-            <li>Gigure out what it is or</li>
-            <li>The links it currently</li>
-            <li>It allows you to start your bid</li>
-            <li>Gigure out what it is or</li>
-            <li>The links it currently</li>
-            <li>It allows you to start your bid</li>
-        </ul> --}}
+            <div class="">
+                <li class="">
+                    <button class="btn" style="background:#E8E8E8;">
+                        <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="icon icon-tabler icons-tabler-outline icon-tabler-arrow-left">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                                <path d="M5 12l14 0" />
+                                <path d="M5 12l6 6" />
+                                <path d="M5 12l6 -6" />
+                            </svg> Kembali</span>
+                    </button>
+                    <button class="btn btn-warning edittask ms-2" id="edit-task">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="-2 -2 24 24">
+                                <path fill="currentColor"
+                                    d="M6 0h8a6 6 0 0 1 6 6v8a6 6 0 0 1-6 6H6a6 6 0 0 1-6-6V6a6 6 0 0 1 6-6m0 2a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V6a4 4 0 0 0-4-4zm6 7h3a1 1 0 0 1 0 2h-3a1 1 0 0 1 0-2m-2 4h5a1 1 0 0 1 0 2h-5a1 1 0 0 1 0-2m0-8h5a1 1 0 0 1 0 2h-5a1 1 0 1 1 0-2m-4.172 5.243L7.95 8.12a1 1 0 1 1 1.414 1.415l-2.828 2.828a1 1 0 0 1-1.415 0L3.707 10.95a1 1 0 0 1 1.414-1.414z" />
+                            </svg>
+                            Edit Tugas
+                        </span>
+                    </button>
+                </li>
+            </div>
+        </ul>
     </div>
 </div>
+
+<div class="tab-content">
+    <div class="tab-pane active" id="detailTask" role="tabpanel">
+        @include('admin.pages.courses.panes.moduls.panes.task.tab-task')
+    </div>
+
+    <div class="tab-pane" id="listCollect" role="tabpanel">
+        @include('admin.pages.courses.panes.moduls.panes.task.tab-collect')
+    </div>
+</div>
+
 @endsection
 
 @section('script')
@@ -104,4 +132,16 @@
     });
 
 </script>
+
+<script>
+    $(document).ready(function() {
+       $(document).on('click', '.detailTask', function() {
+           $('.edittask').removeClass('d-none');
+       });
+ 
+       $(document).on('click', '.listCollect', function() {
+           $('.edittask').addClass('d-none');
+       });
+   });
+ </script>
 @endsection
---
+++ resources/views/admin/pages/courses/panes/moduls/panes/task/tab-task.blade.php
@@ -0,0 +1,27 @@
+<div class="card">
+    <div class="p-4">
+        <div class="d-flex align-items-center gap-2">
+            <div class="d-flex justify-content-center align-items-center" style="background-color: #F6EEFE;border-radius:50%;width:30px;height:30px;">
+                <svg xmlns="http://www.w3.org/2000/svg" style="color: var(--purple-primary);" width="20" height="20" viewBox="-2 -2 24 24">
+                    <path fill="currentColor" d="M6 0h8a6 6 0 0 1 6 6v8a6 6 0 0 1-6 6H6a6 6 0 0 1-6-6V6a6 6 0 0 1 6-6m0 2a4 4 0 0 0-4 4v8a4 4 0 0 0 4 4h8a4 4 0 0 0 4-4V6a4 4 0 0 0-4-4zm6 7h3a1 1 0 0 1 0 2h-3a1 1 0 0 1 0-2m-2 4h5a1 1 0 0 1 0 2h-5a1 1 0 0 1 0-2m0-8h5a1 1 0 0 1 0 2h-5a1 1 0 1 1 0-2m-4.172 5.243L7.95 8.12a1 1 0 1 1 1.414 1.415l-2.828 2.828a1 1 0 0 1-1.415 0L3.707 10.95a1 1 0 0 1 1.414-1.414z" /></svg>
+            </div>
+            <h6 class="mt-1">Tugas 1</h6>
+        </div>
+        <h3 id="title" class="fw-semibold mt-3">Membuat CRUD Blala</h3>
+    </div>
+    <hr>
+    <div class="p-4">
+        <h3>Deskripsi</h3>
+        <p id="description"></p>
+        <hr>
+        {{-- <h4>Unorder list.</h4>
+        <ul style="list-style-type: disc;" class="ms-4 text-black">
+            <li>Gigure out what it is or</li>
+            <li>The links it currently</li>
+            <li>It allows you to start your bid</li>
+            <li>Gigure out what it is or</li>
+            <li>The links it currently</li>
+            <li>It allows you to start your bid</li>
+        </ul> --}}
+    </div>
+</div>
