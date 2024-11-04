8a9ca7a2653e83951a287515f558a9f4e4608394
mohamadarif03 <mohamadarif5392@gmail.com>
2024-11-04 13:03:32 +0700

    Update footer.blade.php

--- resources/views/admin/pages/configuration/footer.blade.php
+++ resources/views/admin/pages/configuration/footer.blade.php
@@ -23,20 +23,12 @@
             </div>
         </div>
     </div>
-
-    <div class="card">
-        <div class="card-header bg-white border-bottom">
-            <h5 class="mb-0">Info</h5>
-        </div>
-        <div class="card-body">
-            <form action="#" enctype="multipart/form-data" id="create-course-form">
-                <div class="row">
-                    <div class="col col-md-12">
-                        <label for="" class="form-label">Logo</label>
-                        <input type="file" name="photo" class="form-control">
-                        <div class="invalid-feedback"></div>
-                    </div>
-                </div>
+    <form action="#" enctype="multipart/form-data" id="create-footer-form">
+        <div class="card">
+            <div class="card-header bg-white border-bottom">
+                <h5 class="mb-0">Info</h5>
+            </div>
+            <div class="card-body">
                 <div class="row mt-3">
                     <div class="col col-md-6">
                         <label for="" class="form-label">Email</label>
@@ -54,64 +46,103 @@
                     <textarea name="description" id="description"></textarea>
                     <div class="invalid-feedback"></div>
                 </div>
-            </form>
-        </div>
-        <div class="card-header bg-white border-bottom">
-            <h5 class="mb-0">Social Media</h5>
-        </div>
-        <div class="card-body">
-            <div class="position-relative overflow-hidden rounded-2" style="background-color: #E8DEF3;">
-                <div class="px-4 py-3">
-                    <div class="row align-items-center">
-                        <div class="col-9">
-                            <h5 class="fw-semibold mb-8" style="color: #9425FE">Informasi</h5>
-                            <h5 class="mb-8">bagian social media inputkan link/url social media anda</h5>
+            </div>
+            <div class="card-header bg-white border-bottom">
+                <h5 class="mb-0">Social Media</h5>
+            </div>
+            <div class="card-body">
+                <div class="position-relative overflow-hidden rounded-2" style="background-color: #E8DEF3;">
+                    <div class="px-4 py-3">
+                        <div class="row align-items-center">
+                            <div class="col-9">
+                                <h5 class="fw-semibold mb-8" style="color: #9425FE">Informasi</h5>
+                                <h5 class="mb-8">bagian social media inputkan link/url social media anda</h5>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col col-md-6">
-                    <label for="" class="form-label">Facebook</label>
-                    <input type="text" class="form-control">
-                    <div class="invalid-feedback"></div>
+                <div class="row mt-3">
+                    <div class="col col-md-6">
+                        <label for="" class="form-label">Facebook</label>
+                        <input type="text" class="form-control">
+                        <div class="invalid-feedback"></div>
+                    </div>
+                    <div class="col col-md-6">
+                        <label for="" class="form-label">Twitter</label>
+                        <input type="text" class="form-control">
+                        <div class="invalid-feedback"></div>
+                    </div>
                 </div>
-                <div class="col col-md-6">
-                    <label for="" class="form-label">Twitter</label>
-                    <input type="text" class="form-control">
-                    <div class="invalid-feedback"></div>
+                <div class="row mt-3">
+                    <div class="col col-md-6">
+                        <label for="" class="form-label">Instagram</label>
+                        <input type="text" class="form-control">
+                        <div class="invalid-feedback"></div>
+                    </div>
+                    <div class="col col-md-6">
+                        <label for="" class="form-label">Linkedin</label>
+                        <input type="text" class="form-control">
+                        <div class="invalid-feedback"></div>
+                    </div>
                 </div>
-            </div>
-            <div class="row mt-3">
-                <div class="col col-md-6">
-                    <label for="" class="form-label">Instagram</label>
-                    <input type="text" class="form-control">
-                    <div class="invalid-feedback"></div>
-                </div>
-                <div class="col col-md-6">
-                    <label for="" class="form-label">Linkedin</label>
-                    <input type="text" class="form-control">
-                    <div class="invalid-feedback"></div>
-                </div>
-            </div>
-            <div class="form-actions mt-4">
-                <div class="text-end">
-                    <button type="reset" class="btn btn-danger text-white font-medium me-2"
-                        onclick="window.history.back();">
-                        Kembali
-                    </button>
-                    <button type="submit" class="btn text-white font-medium" style="background-color: #9425FE">
-                        Tambah
-                    </button>
+                <div class="form-actions mt-4">
+                    <div class="text-end">
+                        <button type="reset" class="btn btn-danger text-white font-medium me-2"
+                            onclick="window.history.back();">
+                            Kembali
+                        </button>
+                        <button type="submit" class="btn text-white font-medium" style="background-color: #9425FE">
+                            Tambah
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </form>
 @endsection
 @push('script')
     <script>
         $('#description').summernote({
             height: 200
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('#create-footer-form').submit(function(e) {
+                e.preventDefault();
+
+                var formData = new FormData(this);
+
+                $.ajax({
+                    type: "POST",
+                    url: "{{ config('app.api_url') }}/api/sub-categories/" + id,
+                    data: formData,
+                    headers: {
+                        Authorization: 'Bearer ' + "{{ session('hummaclass-token') }}"
+                    },
+                    dataType: "json",
+                    contentType: false,
+                    processData: false,
+                    success: function(response) {
+                        Swal.fire({
+                            title: "Sukses",
+                            text: "Berhasil menambah data data.",
+                            icon: "success"
+                        });
+                        $('#modal-create-subcategory').modal('hide');
+                        $('#modal-create-subcategory').find('input').val('');
+
+                        get(1);
+                    },
+                    error: function(response) {
+                        Swal.fire({
+                            title: "Terjadi Kesalahan!",
+                            text: "Ada kesalahan saat menyimpan data.",
+                            icon: "error"
+                        });
+                    }
+                });
+            });
         });
     </script>
 @endpush
