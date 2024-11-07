aa648be4ed43612374d35c842be2108b12c88a8f
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-11-07 14:20:24 +0700

    no image detail barang

--- resources/views/admin/pages/point-exchange/scripts/detail.blade.php
+++ resources/views/admin/pages/point-exchange/scripts/detail.blade.php
@@ -5,9 +5,12 @@
             const name = $(this).data('name')
             const description = $(this).data('name')
             const image = $(this).data('image')
+            const imageUrl = image && /\.(jpeg|jpg|gif|png)$/i.test(image) 
+                ? "{{ config('app.api_url') }}/storage/" + image
+                : "{{ asset('assets/img/no-image/no-image.jpg') }}";
             const stock = $(this).data('stock')
             const points_required = $(this).data('points_required')
-            $('#detailImage').attr('src', "{{ config('app.api_url') }}/storage/" + image);
+            $('#detailImage').attr('src', imageUrl);
             $('#detailName').val(name);
             $('#detailStock').val(stock);
             $('#detailPointsRequired').val(points_required);
