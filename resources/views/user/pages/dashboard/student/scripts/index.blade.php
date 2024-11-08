78ee1b41d2269392cc37af9da11ac3a655890b2f
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-11-08 13:37:00 +0700

    Update index.blade.php

--- resources/views/user/pages/dashboard/student/scripts/index.blade.php
+++ resources/views/user/pages/dashboard/student/scripts/index.blade.php
@@ -38,7 +38,7 @@
 
 
         function cardCourse(data) {
-            // console.log(data.course);
+            console.log(data.course.total_module);
 
             let card = `
             <div class="col-lg-4 col-md-6">
