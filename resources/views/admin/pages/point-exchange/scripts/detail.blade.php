59896e32ce2c246504759d12396af3d227b4e078
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-11-11 11:10:57 +0700

    Update detail.blade.php

--- resources/views/admin/pages/point-exchange/scripts/detail.blade.php
+++ resources/views/admin/pages/point-exchange/scripts/detail.blade.php
@@ -4,9 +4,12 @@
             $('.detailRewardModal').modal('show');
             const name = $(this).data('name')
             const description = $(this).data('name')
+            const image = $(this).data('image');
+            const baseUrl = "{{ config('app.api_url', 'https://core-ecourse.mijurnal.com') }}";
             const imageUrl = image && /\.(jpeg|jpg|gif|png)$/i.test(image)
-            ? "{{ config('app.api_url') }}/storage/" + image : "{{ asset('assets/img/no-image/no-image.jpg') }}";
-                console.log("API URL: " + apiUrl);
+                ? baseUrl + "/storage/" + image 
+                : "{{ asset('assets/img/no-image/no-image.jpg') }}";
+
             const stock = $(this).data('stock')
             const points_required = $(this).data('points_required')
             $('#detailImage').attr('src', imageUrl);
