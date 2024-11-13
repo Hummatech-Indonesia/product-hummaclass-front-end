dfe7f5be9f492e55ca2b8829e28e67f61745e636
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-11-13 13:30:26 +0700

    build

--- resources/js/codex-editor.js
+++ resources/js/codex-editor.js
@@ -6,7 +6,7 @@ import ImageTool from "@editorjs/image";
 import Embed from "@editorjs/embed";
 import RawTool from "@editorjs/raw";
 
-const apiBaseUrl = "https://e-course.mijurnal.com/"; // Ganti dengan URL API Anda
+const apiBaseUrl = "https://e-course.mijurnal.com"; // Ganti dengan URL API Anda
 
 const editor = new EditorJS({
   holder: "editorjs",
