140740b111cbbba76b49a9d325d6e61bd27c6ade
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-11-13 11:14:15 +0700

    detail dashboard top

--- resources/views/user/pages/dashboard/widgets/dashboard-top.blade.php
+++ resources/views/user/pages/dashboard/widgets/dashboard-top.blade.php
@@ -1,20 +1,62 @@
 <div class="dashboard__top-wrap">
-    <div class="dashboard__top-bg banner-user"
+    <div class="dashboard__top-bg banner-user" id="detail-banner-user"
         data-background="{{ isset(session('user')['banner']) ? session('user')['banner'] : asset('assets/img/no-image/no-image.jpg') }}">
     </div>
     <div class="dashboard__instructor-info">
         <div class="dashboard__instructor-info-left">
             <div class="thumb">
-                <img class="detail-photo"
-                    src="{{ isset(session('user')['photo']) ? session('user')['photo'] : asset('assets/img/no-image/no-profile.jpeg') }}"
-                    alt="img">
+                <img class="detail-photo" id="detail-photo-user"
+                    src="" alt="img" style="width: 120px;height: 120px;object-fit: cover;">
             </div>
             <div class="content">
-                <h4 class="title detail-name"></h4>
+                <h4 class="title detail-name" id="detail-name-user"></h4>
                 <div class="review__wrap review__wrap-two">
-                    <span>{{ session('user')['email'] }}</span>
+                    <span id="detail-email-user"></span>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "{{ config('app.api_url') }}" + "/api/profile" ,
+            headers: {
+                Authorization: 'Bearer ' + "{{ session('hummaclass-token') }}"
+            },
+            dataType: "json",
+            success: function(response) {
+
+                console.log(response);
+                
+                // const photo = response.data.thumbnail && /\.(jpeg|jpg|gif|png)$/i.test(response.data
+                //         .thumbnail) ?
+                //     response.data.thumbnail :
+                //     "{{ asset('assets/img/no-image/no-image.jpg') }}";
+                // $('#detail-photo-user').attr('src', response.data.photo);
+
+                var profileImage = response.data.photo && /\.(jpeg|jpg|gif|png)$/i.test(response.data.photo) 
+                    ? response.data.photo
+                    : '{{ asset('assets/img/no-image/no-profile.jpeg') }}';
+                $('#detail-photo-user').attr('src', profileImage);
+                var bannerImage = data.banner && /\.(jpeg|jpg|gif|png)$/i.test(data.banner)
+                    ? data.banner
+                    : '{{ asset('assets/img/no-image/no-image.jpg') }}';
+                $('#detail-banner-user').css('background-image', 'url(' + bannerImage + ')');
+                $('#detail-name-user').html(response.data.name);
+                $('#detail-email-user').html(response.data.email);
+                // $('#detail-created').html(response.data.created);
+                // $('#detail-description').html(response.data.description);
+            },
+            error: function(xhr) {
+                Swal.fire({
+                    title: "Terjadi Kesalahan!",
+                    text: "Tidak dapat memuat data modul.",
+                    icon: "error"
+                });
+            }
+        });
+    });
+</script>
