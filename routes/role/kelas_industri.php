6212fc552ab22a72d85244da4fe23e07e70c1bca
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-11-13 10:34:05 +0700

    route kelas industri

--- routes/role/kelas_industri.php
+++ routes/role/kelas_industri.php
@@ -1,3 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth_custom', 'admin'])->prefix('admin')->name('admin.')->group(function () {
+
+    // kelas industri
+    Route::prefix('class')->name('class.')->group(function () {
+        Route::prefix('school')->name('school.')->group(function () {
+            Route::get('/', function () {
+                return view('Kelas-Industri.admin.school.index');
+            })->name('index');
+            Route::get('/{slug}', function ($slug) {
+                return view('Kelas-Industri.admin.school.details.detail', compact('slug'));
+            })->name('show');
+        });
+        Route::get('/create-school', function () {
+            return view('Kelas-Industri.admin.school.create');
+        })->name('school.create');
+        Route::get('/edit-school/{id}', function ($id) {
+            return view('Kelas-Industri.admin.school.edit', compact('id'));
+        })->name('school.edit');
+    });
+});
