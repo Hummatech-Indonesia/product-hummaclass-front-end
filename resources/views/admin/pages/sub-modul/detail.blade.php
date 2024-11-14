6bf906231f2e4ef85c0eb1276f4a3d5fa83cba53
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-11-14 13:46:18 +0700

    Update detail.blade.php

--- resources/views/admin/pages/sub-modul/detail.blade.php
+++ resources/views/admin/pages/sub-modul/detail.blade.php
@@ -66,7 +66,7 @@
                                 </svg>
                                 </svg> Kembali</span>
                         </a>
-                        <button class="btn btn-warning editDescription">
+                        <a href="{{ route('admin.edit-materi.index', $id) }}" class="btn btn-warning editDescription">
                             <span><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                     fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                     stroke-linejoin="round"
@@ -75,7 +75,7 @@
                                     <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
                                     <path d="M13.5 6.5l4 4" />
                                 </svg> Edit Materi</span>
-                        </button>
+                        </a>
                     </li>
                 </div>
             </ul>
