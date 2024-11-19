1d15aee9f7e022c8aac4210d0bd6ad44d52a14e0
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-11-19 14:46:05 +0700

    Update header.blade.php

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -345,26 +345,18 @@
                 },
                 dataType: "json",
                 success: function(response) {
-                    console.log({
-                        response
-                    })
-                    var profileUser = response.data.photo && /\.(jpeg|jpg|gif|png)$/i.test(
+                    var profileImage1 = response.data.photo && /\.(jpeg|jpg|gif|png)$/i.test(
                             response.data.photo) ?
                         response.data.photo :
                         '{{ asset('assets/img/no-image/no-profile.jpeg') }}';
-
-                    console.log(profileUser);
-
-                    $('.photo-user').attr('src', profileUser);
+                    $('.photo-user').attr('src', profileImage1);
                 },
                 error: function(xhr) {
-                    if (response.status != 401) {
-                        Swal.fire({
-                            title: "Terjadi Kesalahan!",
-                            text: "Tidak dapat memuat data profil.",
-                            icon: "error"
-                        });
-                    }
+                    Swal.fire({
+                        title: "Terjadi Kesalahan!",
+                        text: "Tidak dapat memuat data profil.",
+                        icon: "error"
+                    });
                 }
             });
         @endif
