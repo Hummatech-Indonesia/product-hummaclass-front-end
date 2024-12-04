1e344d3de333bda2ccfd94704eb8e2b25644f56d
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-12-04 15:24:24 +0700

    Update index.blade.php

--- resources/views/Kelas-Industri/admin/learning-paths/index.blade.php
+++ resources/views/Kelas-Industri/admin/learning-paths/index.blade.php
@@ -150,58 +150,72 @@
             }
 
             function courseLearningPathList(index, value) {
+                let price;
+
+                const formatRupiah = (value) => {
+                    return `Rp ${parseInt(value, 10).toLocaleString('id-ID')}`;
+                };
+
+                price =
+                    value.course.is_premium == 1 ?
+                    (value.course.promotional_price != null && value.course.promotional_price > 0 ?
+                        formatRupiah(value.course.promotional_price) :
+                        value.course.promotional_price === 0 ?
+                        'Gratis' :
+                        value.course.price != null && value.course.price > 0 ?
+                        formatRupiah(value.course.price) :
+                        'Gratis') :
+                    'Gratis';
+
+
                 return `
-                <div class="card input-group position-relative">
-                    <div class="card-body align-items-center">
-                        <div class="d-flex justify-content-between align-items-center mb-3">
-                            <h5><b>Langkah ${index+1}</b></h5>
-                        </div>
-                        <div class="row mt-2">
-                            <div class="col-5">
-                                <img src="{{ asset('assets/img/courses/course_thumb01.jpg') }}" alt="kursus.jpg"
-                                    class="img-fluid rounded">
-                            </div>
-                            <div class="col-7">
-                                <div class="d-flex gap-2 align-items-center">
-                                    <div class="p-1 rounded text-center" style="background: #F6EEFE;color:#9425FE;">
-                                        <span>Development</span>
-                                    </div>
-                                    <div class="text-center">
-                                        <img src="{{ asset('admin/dist/images/profile/user-1.jpg') }}" alt="user.jpg"
-                                            class="rounded-circle" style="height: 24px;width:24px;">
-                                        <span class="text-muted"> David Millar</span>
-                                    </div>
+                        <div class="card input-group position-relative">
+                            <div class="card-body align-items-center">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <h5><b>Langkah ${index + 1}</b></h5>
                                 </div>
-                                <h4><b>Learning Javascript with Imagination</b></h4>
-                                <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                    Amet,
-                                    facere
-                                    corporis, ullam voluptatibus hic beatae ducimus aspernatur debitis nihil autem
-                                    placeat?
-                                    Deserunt saepe, optio enim corporis beatae nesciunt commodi nihil!</p>
-                                <h4 style="color: #9425FE;"><b>Rp. 300.000</b> / <span class="fs-2 text-dark"><i
-                                            class="fa fa-star fa-md text-warning"></i> (4,5
-                                        Reviews)</span></h4>
-                                <div class="d-flex gap-2 align-items-center">
-                                    <div class="p-2 rounded" style="background: #FEF5EE;">
-                                        <div class="d-flex gap-2"><i style="color: #FFB649;" class="fa fa-book fa-md"></i>
-                                            <b>8 Modul</b>
-                                        </div>
+                                <div class="row mt-2">
+                                    <div class="col-5">
+                                        <img src="{{ asset('assets/img/courses/course_thumb01.jpg') }}" alt="kursus.jpg"
+                                            class="img-fluid rounded">
                                     </div>
-                                    <div class="p-2 rounded" style="background: #FEF5EE;">
-                                        <div class="d-flex gap-2"><i style="color: #FFB649;" class="fa fa-folder fa-md"></i>
-                                            <b>1 Tugas Akhir</b>
+                                    <div class="col-7">
+                                        <div class="d-flex gap-2 align-items-center">
+                                            <div class="p-1 rounded text-center" style="background: #F6EEFE;color:#9425FE;">
+                                                <span>${value.course.sub_category.name}</span>
+                                            </div>
+                                            <div class="text-center">
+                                                <img src="{{ asset('admin/dist/images/profile/user-1.jpg') }}" alt="user.jpg"
+                                                    class="rounded-circle" style="height: 24px;width:24px;">
+                                                <span class="text-muted"> ${value.course.user.name}</span>
+                                            </div>
+                                        </div>
+                                        <h4><b>${value.course.title}</b></h4>
+                                        <p class="text-muted">${value.course.description}</p>
+                                        <h4 style="color: #9425FE;"><b>${price}</b> / <span class="fs-2 text-dark"><i
+                                                    class="fa fa-star fa-md text-warning"></i> (${value.course.rating}
+                                                Reviews)</span></h4>
+                                        <div class="d-flex gap-2 align-items-center">
+                                            <div class="p-2 rounded" style="background: #FEF5EE;">
+                                                <div class="d-flex gap-2"><i style="color: #FFB649;" class="fa fa-book fa-md"></i>
+                                                    <b>${value.course.module_count} Modul</b>
+                                                </div>
+                                            </div>
+                                            <div class="p-2 rounded" style="background: #FEF5EE;">
+                                                <div class="d-flex gap-2"><i style="color: #FFB649;" class="fa fa-folder fa-md"></i>
+                                                    <b>1 Tugas Akhir</b>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
+                            <button class="btn ms-auto position-absolute" style="height: 100%; right:0;background:#ECECEC;"><i
+                                    class="fa fa-ellipsis-v"></i></button>
                         </div>
-                    </div>
-                    <button class="btn ms-auto position-absolute" style="height: 100%; right:0;background:#ECECEC;"><i
-                            class="fa fa-ellipsis-v"></i></button>
-                </div>
-                `
+                        `;
             }
+
 
             function getCourseLearningPath(class_level, division_id) {
                 $.ajax({
