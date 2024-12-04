990c9f8a57a2b7adb1ed7dd2f8c8d3abcdc287f4
frendikawa <148562963+frendikawa@users.noreply.github.com>
2024-12-04 14:19:41 +0700

    getdivision

--- resources/views/Kelas-Industri/admin/learning-paths/index.blade.php
+++ resources/views/Kelas-Industri/admin/learning-paths/index.blade.php
@@ -40,17 +40,20 @@
         <div class="card-body">
             <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item">
-                    <a class="nav-link active" data-bs-toggle="tab" href="#home" role="tab" data-class_level="10">
+                    <a class="nav-link nav-class_level-link active" data-bs-toggle="tab" href="#home" role="tab"
+                        data-class_level="10">
                         <span>Kelas 10</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="tab" href="#profile" role="tab" data-class_level="11">
+                    <a class="nav-link nav-class_level-link" data-bs-toggle="tab" href="#profile" role="tab"
+                        data-class_level="11">
                         <span>Kelas 11</span>
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" data-bs-toggle="tab" href="#messages" role="tab" data-class_level="12">
+                    <a class="nav-link nav-class_level-link" data-bs-toggle="tab" href="#messages" role="tab"
+                        data-class_level="12">
                         <span>Kelas 12</span>
                     </a>
                 </li>
@@ -77,65 +80,55 @@
                 <button class="btn text-white" style="background: #9425FE;"><i class="fa fa-plus fa-md"></i>
                     Tambah</button>
             </div>
-            <div class="tab-content" id="v-pills-tabContent">
-                <div class="course-learning-path-list">
-
-                </div>
-                <div class="tab-pane fade show active" id="v-pills-web-technology" role="tabpanel"
-                    aria-labelledby="v-pills-web-technology-tab">
-                    <div class="card input-group position-relative">
-                        <div class="card-body d-flex justify-content-between align-items-center">
-                            <div id="course-learning-path-list">
-                                <div class="d-flex justify-content-between align-items-center mb-3">
-                                    <h5><b>Langkah 1</b></h5>
-                                </div>
-                                <div class="row mt-2">
-                                    <div class="col-5">
-                                        <img src="{{ asset('assets/img/courses/course_thumb01.jpg') }}" alt="kursus.jpg"
-                                            class="img-fluid rounded">
+            <div id="course-learning-path-list">
+                <div class="card input-group position-relative">
+                    <div class="card-body align-items-center">
+                        <div class="d-flex justify-content-between align-items-center mb-3">
+                            <h5><b>Langkah 1</b></h5>
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-5">
+                                <img src="{{ asset('assets/img/courses/course_thumb01.jpg') }}" alt="kursus.jpg"
+                                    class="img-fluid rounded">
+                            </div>
+                            <div class="col-7">
+                                <div class="d-flex gap-2 align-items-center">
+                                    <div class="p-1 rounded text-center" style="background: #F6EEFE;color:#9425FE;">
+                                        <span>Development</span>
                                     </div>
-                                    <div class="col-7">
-                                        <div class="d-flex gap-2 align-items-center">
-                                            <div class="p-1 rounded text-center" style="background: #F6EEFE;color:#9425FE;">
-                                                <span>Development</span>
-                                            </div>
-                                            <div class="text-center">
-                                                <img src="{{ asset('admin/dist/images/profile/user-1.jpg') }}"
-                                                    alt="user.jpg" class="rounded-circle" style="height: 24px;width:24px;">
-                                                <span class="text-muted"> David Millar</span>
-                                            </div>
+                                    <div class="text-center">
+                                        <img src="{{ asset('admin/dist/images/profile/user-1.jpg') }}" alt="user.jpg"
+                                            class="rounded-circle" style="height: 24px;width:24px;">
+                                        <span class="text-muted"> David Millar</span>
+                                    </div>
+                                </div>
+                                <h4><b>Learning Javascript with Imagination</b></h4>
+                                <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                                    Amet,
+                                    facere
+                                    corporis, ullam voluptatibus hic beatae ducimus aspernatur debitis nihil autem
+                                    placeat?
+                                    Deserunt saepe, optio enim corporis beatae nesciunt commodi nihil!</p>
+                                <h4 style="color: #9425FE;"><b>Rp. 300.000</b> / <span class="fs-2 text-dark"><i
+                                            class="fa fa-star fa-md text-warning"></i> (4,5
+                                        Reviews)</span></h4>
+                                <div class="d-flex gap-2 align-items-center">
+                                    <div class="p-2 rounded" style="background: #FEF5EE;">
+                                        <div class="d-flex gap-2"><i style="color: #FFB649;" class="fa fa-book fa-md"></i>
+                                            <b>8 Modul</b>
                                         </div>
-                                        <h4><b>Learning Javascript with Imagination</b></h4>
-                                        <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                            Amet,
-                                            facere
-                                            corporis, ullam voluptatibus hic beatae ducimus aspernatur debitis nihil autem
-                                            placeat?
-                                            Deserunt saepe, optio enim corporis beatae nesciunt commodi nihil!</p>
-                                        <h4 style="color: #9425FE;"><b>Rp. 300.000</b> / <span class="fs-2 text-dark"><i
-                                                    class="fa fa-star fa-md text-warning"></i> (4,5
-                                                Reviews)</span></h4>
-                                        <div class="d-flex gap-2 align-items-center">
-                                            <div class="p-2 rounded" style="background: #FEF5EE;">
-                                                <div class="d-flex gap-2"><i style="color: #FFB649;"
-                                                        class="fa fa-book fa-md"></i>
-                                                    <b>8 Modul</b>
-                                                </div>
-                                            </div>
-                                            <div class="p-2 rounded" style="background: #FEF5EE;">
-                                                <div class="d-flex gap-2"><i style="color: #FFB649;"
-                                                        class="fa fa-folder fa-md"></i>
-                                                    <b>1 Tugas Akhir</b>
-                                                </div>
-                                            </div>
+                                    </div>
+                                    <div class="p-2 rounded" style="background: #FEF5EE;">
+                                        <div class="d-flex gap-2"><i style="color: #FFB649;" class="fa fa-folder fa-md"></i>
+                                            <b>1 Tugas Akhir</b>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <button class="btn ms-auto position-absolute" style="height: 100%; right:0;background:#ECECEC;"><i
-                                class="fa fa-ellipsis-v"></i></button>
                     </div>
+                    <button class="btn ms-auto position-absolute" style="height: 100%; right:0;background:#ECECEC;"><i
+                            class="fa fa-ellipsis-v"></i></button>
                 </div>
             </div>
         </div>
@@ -146,22 +139,25 @@
         $(document).ready(function() {
             function divisionList(index, value) {
                 return `
-                <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                            <a class="nav-link ${index == 0 ? 'active' : ''}" id="v-pills-${value.division.name}-tab" data-bs-toggle="pill"
-                                href="#v-pills-${value.division.name}" role="tab" aria-controls="v-pills-${value.division.name}"
-                                aria-selected="true">
-                                ${value.division.name}
-                            </a>
-                        </div>
+                <div class="nav flex-column nav-pills nav-division_id-link" data-division_id="${value.division.id}" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <a class="nav-link ${index == 0 ? 'active' : ''}" id="v-pills-${value.division.name}-tab" data-bs-toggle="pill"
+                        href="#v-pills-${value.division.name}" role="tab" aria-controls="v-pills-${value.division.name}"
+                        aria-selected="true">
+                        ${value.division.name}
+                    </a>
+                </div>
                 `
             }
 
-            function getCourseLearningPath() {
+            function getDivision(class_level) {
                 $.ajax({
                     type: "GET",
                     url: "{{ config('app.api_url') }}/api/learning-paths",
                     headers: {
                         Authorization: 'Bearer ' + "{{ session('hummaclass-token') }}",
+                    },
+                    data: {
+                        class_level: class_level
                     },
                     dataType: "json",
                     success: function(response) {
@@ -170,7 +166,7 @@
                             $('#division-tab-list').append(divisionList(indexInArray,
                                 valueOfElement));
                         });
-                        alert('berhasil fetch data');
+                        let division_id = $('.nav-division_id-link.active').data('division_id')
                     },
                     error: function(xhr) {
                         Swal.fire({
@@ -182,10 +178,15 @@
                 });
             }
 
-            getCourseLearningPath();
+            let class_level = $('.nav-class_level-link.active').data('class_level')
 
-            
+            getDivision(class_level);
 
+            $(document).on('click', '.nav-class_level-link', function() {
+                $('.nav-class_level-link').removeClass('active')
+                $(this).addClass('active')
+                getDivision($(this).data('class_level'))
+            })
         });
     </script>
 @endsection
