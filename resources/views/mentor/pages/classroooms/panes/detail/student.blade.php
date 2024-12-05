4597b28406870f90e33fb11cda8307e7960f3ea5
Alfian <112760996+alfian8752@users.noreply.github.com>
2024-12-05 10:14:53 +0700

    list attendances

--- resources/views/mentor/pages/classroooms/panes/detail/student.blade.php
+++ resources/views/mentor/pages/classroooms/panes/detail/student.blade.php
@@ -1,11 +1,12 @@
 <div class="card">
     <div class="card-body">
         <h5 class="card-title">Daftar Siswa</h5>
-        <form action="">
+        <form action="" id="search-form">
             <div class="d-flex gap-3 mt-2 mb-3">
                 <div class="position-relative">
                     <input type="text" class="form-control product-search px-4 ps-5" style="background-color: #fff"
-                        name="name" value="{{ old('name', request('name')) }}" id="input-search" placeholder="Search">
+                        name="name" value="{{ old('name', request('name')) }}" id="input-search"
+                        placeholder="Search">
                     <i class="ti ti-search position-absolute top-50 start-0 translate-middle-y fs-6 ms-3"
                         style="color: #8B8B8B"></i>
                 </div>
@@ -29,77 +30,7 @@
     </div>
 </div>
 @push('script')
-    <script>
-        const data = [{
-            student: "Lukman",
-            email: "email",
-            gender: "Laki",
-            nisn: "216392",
-        }]
-        $.each(data, function(index, value) {
-            $('#tableBody').append(studentClassroom(index,
-                value));
-        });
-
-        function studentClassroom(index, value) {
-            return `
-                <tr class="fw-semibold">
-                    <td>
-                        <div class="d-flex align-items-center">
-                            <img src="{{ asset('assets/img/no-image/no-profile.jpeg') }}"
-                                class="rounded-circle me-2 user-profile" style="object-fit: cover" width="40"
-                                height="40" alt="">
-                            <div class="ms-3">
-                                <h6 class="fs-4 fw-semibold mb-0">${value.student}</h6>
-                                <span class="fw-normal">${value.email}</span>
-                            </div>
-                        </div>
-                    </td>
-                    <td>${value.gender}</td>
-                    <td>${value.nisn}</td>
-                    <td>
-                        <div class="d-flex gap-1">
-                            <button class="btn btn-sm text-white" data-bs-toggle="modal"
-                                data-bs-target="#modal-detail-student"
-                                style="background-color: #9425FE">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                    viewBox="0 0 24 24">
-                                    <g fill="none" stroke="currentColor" stroke-linecap="round"
-                                        stroke-linejoin="round" stroke-width="2">
-                                        <path d="M3 13c3.6-8 14.4-8 18 0" />
-                                        <path d="M12 17a3 3 0 1 1 0-6a3 3 0 0 1 0 6" />
-                                    </g>
-                                </svg>
-                            </button>
-                            <button class="btn btn-sm btn-danger text-white" data-bs-toggle="modal"
-                                data-bs-target="#modal-detail-student">
-                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                                    class="icon icon-tabler icons-tabler-outline icon-tabler-trash">
-                                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                    <path d="M4 7l16 0" />
-                                    <path d="M10 11l0 6" />
-                                    <path d="M14 11l0 6" />
-                                    <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />
-                                    <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" />
-                                </svg>
-                            </button>
-                            <button class="btn btn-sm btn-warning text-white" data-bs-toggle="modal"
-                                data-bs-target="#modal-detail-student">
-                                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"
-                                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                        stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" />
-                                        <path d="M13.5 6.5l4 4" />
-                                    </svg>
-                            </button>
-                        </div>
-                    </td>
-                </tr>
-            `
-        }
-    </script>
+    <script></script>
     {{-- <script>
         $(document).ready(function() {
             var slug = "{{ $slug }}"
