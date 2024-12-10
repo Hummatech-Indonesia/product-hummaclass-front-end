be5c76326f7c6501b9d5967a2f4e814191ea3582
nesaathussholeha <nesaathussholeha@gmail.com>
2024-12-10 12:18:41 +0700

    slicing dashboard student

--- resources/views/student/pages/dashboard/index.blade.php
+++ resources/views/student/pages/dashboard/index.blade.php
@@ -1,81 +1,67 @@
 @extends('student.layouts.app')
+@section('style')
+    <style>
+        .bg-primary {
+            background-color: #9425FE !important;
+        }
+
+        .text-primary {
+            color: #9425FE !important;
+        }
+
+        .bg-light-primary {
+            background-color: #F6EEFE !important;
+        }
+
+        .text-bg-purple {
+            color: var(--purple-primary) !important;
+            background: var(--purple-light-primary) !important;
+        }
+    </style>
+@endsection
 @section('content')
-<div class="col-lg-9">
-    <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="bg-white border dashboard__counter-item d-flex align-items-center gap-3"
-                style="padding: 25px;">
-                <div class="icon" style="background:#9424FE;margin: 0;width: 75px;height: 75px;">
-                    <i class="text-white skillgro-book"></i>
+    <div class="col-lg-12">
+        <div class="swiper dashboard-courses-active mb-3">
+            @include('student.pages.dashboard.swiper')
+        </div>
+
+        <div class="card border  rounded-3 p-3 mb-3">
+            <div class="card-body">
+                <h5>Tagihan Anda Semester Ini</h5>
+
+                <div class="d-flex align-items-center justify-content-between mb-3">
+                    <h1 style="color: #9425FE">Rp. 100.000</h1>
+                    <span class="p-3 rounded-3" style="color: #9425FE; background-color: #F6EEFE">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M13.5 16a1.5 1.5 0 1 1-3 0a1.5 1.5 0 0 1 3 0" />
+                            <path fill="currentColor"
+                                d="m14.347.66l3.18 4.456l2.097-.715L21.538 10h.962v12h-21V10h.51v-.01l.648.006zM9.397 10h10.028l-1.037-3.033l-1.522.487zM7.839 8.417L15.55 5.79l-1.604-2.25zM5.5 12h-2v2a2 2 0 0 0 2-2m10 4a3.5 3.5 0 1 0-7 0a3.5 3.5 0 0 0 7 0m5 4v-2a2 2 0 0 0-2 2zm-2-8a2 2 0 0 0 2 2v-2zm-15 8h2a2 2 0 0 0-2-2z" />
+                        </svg>
+                    </span>
                 </div>
-                <div class="content" style="margin: 0;text-align: left;">
-                    <span class="count odometer fs-3" data-count="10" style="color: black"></span>
-                    <p style="color: black">Tugas</p>
+
+                <div class="border mb-3"></div>
+
+                <div class="row">
+                    <div class="col-12 col-lg-8">
+                        <p>Anda harus melunasi tagihan anda, untuk bisa melanjutkan materi</p>
+                    </div>
+                    <div class="col-12 col-lg-4 text-end">
+                        <p style="color: #9425FE">
+                            <span>Klik disini untuk membayar</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path fill="currentColor"
+                                    d="M17.92 11.62a1 1 0 0 0-.21-.33l-5-5a1 1 0 0 0-1.42 1.42l3.3 3.29H7a1 1 0 0 0 0 2h7.59l-3.3 3.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l5-5a1 1 0 0 0 .21-.33a1 1 0 0 0 0-.76" />
+                            </svg>
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="bg-white border dashboard__counter-item d-flex align-items-center gap-3"
-                style="padding: 25px;">
-                <div class="icon" style="background:#9424FE;margin: 0;width: 75px;height: 75px;">
-                    <i class="text-white skillgro-book"></i>
-                </div>
-                <div class="content" style="margin: 0;text-align: left;">
-                    <span class="count odometer fs-3" data-count="10" style="color: black"></span>
-                    <p style="color: black">Materi</p>
-                </div>
-            </div>
+
+        <div class="row">
+            @include('student.pages.dashboard.task-statistics')
         </div>
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="bg-white border dashboard__counter-item d-flex align-items-center gap-3"
-                style="padding: 25px;">
-                <div class="icon" style="background:#9424FE;margin: 0;width: 75px;height: 75px;">
-                    <i class="text-white skillgro-book"></i>
-                </div>
-                <div class="content" style="margin: 0;text-align: left;">
-                    <span class="count odometer fs-3" data-count="10" style="color: black"></span>
-                    <p style="color: black">Tantangan</p>
-                </div>
-            </div>
-        </div>
+
     </div>
-    <div class="row">
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="bg-white border dashboard__counter-item d-flex align-items-center gap-3"
-                style="padding: 25px;">
-                <div class="icon" style="background:#9424FE;margin: 0;width: 75px;height: 75px;">
-                    <i class="text-white skillgro-book"></i>
-                </div>
-                <div class="content" style="margin: 0;text-align: left;">
-                    <span class="count odometer fs-3" data-count="10" style="color: black"></span>
-                    <p style="color: black">Event</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="bg-white border dashboard__counter-item d-flex align-items-center gap-3"
-                style="padding: 25px;">
-                <div class="icon" style="background:#9424FE;margin: 0;width: 75px;height: 75px;">
-                    <i class="text-white skillgro-book"></i>
-                </div>
-                <div class="content" style="margin: 0;text-align: left;">
-                    <span class="count odometer fs-3" data-count="10" style="color: black"></span>
-                    <p style="color: black">Point</p>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-4 col-sm-6">
-            <div class="bg-white border dashboard__counter-item d-flex align-items-center gap-3"
-                style="padding: 25px;">
-                <div class="icon" style="background:#9424FE;margin: 0;width: 75px;height: 75px;">
-                    <i class="text-white skillgro-book"></i>
-                </div>
-                <div class="content" style="margin: 0;text-align: left;">
-                    <span class="count odometer fs-3" data-count="10" style="color: black"></span>
-                    <p style="color: black">Tagihan Semester Ini</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
---
+++ resources/views/student/pages/dashboard/task-statistics.blade.php
@@ -0,0 +1,112 @@
+<div class="col-12 col-lg-6 mb-3">
+    <div class="card border p-2">
+        <div class="card-body">
+            <h4>Tugas</h4>
+            <p>Statistik Tugas</p>
+
+            <div class="p-4 rounded-3 bg-light-primary mb-3">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h2 class="mb-0 me-2 text-primary">20</h2>
+                        <p class="mb-0 me-2">sudah dikerjakan</p>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="45"
+                            height="45" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M20.5 11H19V7a2 2 0 0 0-2-2h-4V3.5A2.5 2.5 0 0 0 10.5 1A2.5 2.5 0 0 0 8 3.5V5H4a2 2 0 0 0-2 2v3.8h1.5c1.5 0 2.7 1.2 2.7 2.7S5 16.2 3.5 16.2H2V20a2 2 0 0 0 2 2h3.8v-1.5c0-1.5 1.2-2.7 2.7-2.7s2.7 1.2 2.7 2.7V22H17a2 2 0 0 0 2-2v-4h1.5a2.5 2.5 0 0 0 2.5-2.5a2.5 2.5 0 0 0-2.5-2.5" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+
+            <div class="p-4 rounded-3 bg-light-primary mb-3">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h2 class="mb-0 me-2 text-primary">20</h2>
+                        <p class="mb-0 me-2">Belum dikerjakan</p>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="45"
+                            height="45" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="m23.5 17l-5 5l-3.5-3.5l1.5-1.5l2 2l3.5-3.5zm-3-6a2.5 2.5 0 0 1 2.5 2.5c0 .31-.06.61-.16.89C21.8 13.5 20.46 13 19 13c-3.31 0-6 2.69-6 6v.54c-.36-1.04-1.35-1.74-2.5-1.74c-1.5 0-2.7 1.2-2.7 2.7V22H4c-1.1 0-2-.9-2-2v-3.8h1.5c1.5 0 2.7-1.2 2.7-2.7S5 10.8 3.5 10.8H2V7a2 2 0 0 1 2-2h4V3.5C8 2.12 9.12.998 10.5.998S13 2.12 13 3.5V5h4a2 2 0 0 1 2 2v4z" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+            <div class="p-4 rounded-3 bg-light-primary mb-3">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h2 class="mb-0 me-2 text-primary">20</h2>
+                        <p class="mb-0 me-2">Tidak dikerjakan</p>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="45"
+                            height="45" viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M13.04 19.61a2.68 2.68 0 0 0-2.54-1.81c-1.5 0-2.7 1.2-2.7 2.7V22H4a2 2 0 0 1-2-2v-3.8h1.5c1.5 0 2.7-1.2 2.7-2.7S5 10.8 3.5 10.8H2V7c0-1.1.9-2 2-2h4V3.5a2.5 2.5 0 0 1 5 0V5h4a2 2 0 0 1 2 2v4h1.5a2.5 2.5 0 0 1 2.5 2.5c0 .32-.06.62-.17.89A6 6 0 0 0 19 13c-3.31 0-6 2.69-6 6c0 .2 0 .41.04.61m8.08-4.15L19 17.59l-2.12-2.13l-1.41 1.42L17.59 19l-2.12 2.12l1.41 1.42L19 20.41l2.12 2.13l1.42-1.42L20.41 19l2.13-2.12z" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+<div class="col-12 col-lg-6 mb-3">
+    <div class="card border p-2">
+        <div class="card-body">
+            <h4>Tantangan</h4>
+            <p>Statistik Tantangan</p>
+
+            <div class="p-4 rounded-3 bg-light-primary mb-3">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h2 class="mb-0 me-2 text-primary">20</h2>
+                        <p class="mb-0 me-2">sudah dikerjakan</p>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="45" height="45"
+                            viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M13.75 3.5h-3.5a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5m0-1.5c1.14 0 2.08.846 2.23 1.945l.013.135l-.007-.08h1.764A2.25 2.25 0 0 1 20 6.25v13.5A2.25 2.25 0 0 1 17.75 22H6.25A2.25 2.25 0 0 1 4 19.75V6.25A2.25 2.25 0 0 1 6.25 4h1.764l-.008.08l.015-.135A2.25 2.25 0 0 1 10.25 2zM14 17H8a.75.75 0 0 0 0 1.5h6a.75.75 0 0 0 0-1.5m-2-4H8a.75.75 0 0 0 0 1.5h4a.75.75 0 0 0 0-1.5m4-4H8a.75.75 0 0 0 0 1.5h8A.75.75 0 0 0 16 9" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+
+            <div class="p-4 rounded-3 bg-light-primary mb-3">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h2 class="mb-0 me-2 text-primary">20</h2>
+                        <p class="mb-0 me-2">Belum dikerjakan</p>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="45" height="45"
+                            viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M13.75 3.5h-3.5a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5m0-1.5c1.14 0 2.08.846 2.23 1.945l.013.135l-.007-.08h1.764A2.25 2.25 0 0 1 20 6.25v13.5A2.25 2.25 0 0 1 17.75 22H6.25A2.25 2.25 0 0 1 4 19.75V6.25A2.25 2.25 0 0 1 6.25 4h1.764l-.008.08l.015-.135A2.25 2.25 0 0 1 10.25 2zM14 17H8a.75.75 0 0 0 0 1.5h6a.75.75 0 0 0 0-1.5m-2-4H8a.75.75 0 0 0 0 1.5h4a.75.75 0 0 0 0-1.5m4-4H8a.75.75 0 0 0 0 1.5h8A.75.75 0 0 0 16 9" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+            <div class="p-4 rounded-3 bg-light-primary mb-3">
+                <div class="d-flex align-items-center justify-content-between">
+                    <div>
+                        <h2 class="mb-0 me-2 text-primary">20</h2>
+                        <p class="mb-0 me-2">Tidak dikerjakan</p>
+                    </div>
+                    <span>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="text-primary" width="45" height="45"
+                            viewBox="0 0 24 24">
+                            <path fill="currentColor"
+                                d="M13.75 3.5h-3.5a.75.75 0 0 0 0 1.5h3.5a.75.75 0 0 0 0-1.5m0-1.5c1.14 0 2.08.846 2.23 1.945l.013.135l-.007-.08h1.764A2.25 2.25 0 0 1 20 6.25v13.5A2.25 2.25 0 0 1 17.75 22H6.25A2.25 2.25 0 0 1 4 19.75V6.25A2.25 2.25 0 0 1 6.25 4h1.764l-.008.08l.015-.135A2.25 2.25 0 0 1 10.25 2zM14 17H8a.75.75 0 0 0 0 1.5h6a.75.75 0 0 0 0-1.5m-2-4H8a.75.75 0 0 0 0 1.5h4a.75.75 0 0 0 0-1.5m4-4H8a.75.75 0 0 0 0 1.5h8A.75.75 0 0 0 16 9" />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
