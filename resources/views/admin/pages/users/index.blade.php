f6d3a39817ebe71990f386360e4ade6f0d7f62c1
nesaathussholeha <nesaathussholeha@gmail.com>
2024-12-11 14:52:20 +0700

    bug sidebar menu user role admin

--- resources/views/admin/pages/users/index.blade.php
+++ resources/views/admin/pages/users/index.blade.php
@@ -1,12 +1,12 @@
 @extends('admin.layouts.app')
 
 @section('style')
-    <style>
+    {{-- <style>
         .btn-close {
             --bs-btn-close-bg: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath d='M.293.293a1 1 0 0 1 1.414 0L8 6.586 14.293.293a1 1 0 1 1 1.414 1.414L9.414 8l6.293 6.293a1 1 0 0 1-1.414 1.414L8 9.414l-6.293 6.293a1 1 0 0 1-1.414-1.414L6.586 8 .293 1.707a1 1 0 0 1 0-1.414z'/%3e%3c/svg%3e");
             background: transparent var(--bs-btn-close-bg) center/1em auto no-repeat;
         }
-    </style>
+    </style> --}}
 @endsection
 
 @section('content')
@@ -74,7 +74,7 @@
     <x-delete-modal-component />
 @endsection
 @section('script')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    {{-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> --}}
     <script>
         let debounceTimer;
         $('#search-name').keyup(function() {
@@ -108,7 +108,7 @@
                 error: function(xhr) {
                     Swal.fire({
                         title: "Terjadi Kesalahan!",
-                        text: "Tidak dapat memuat data kategori.",
+                        text: "Tidak dapat memuat data profil.",
                         icon: "error"
                     });
                 }
