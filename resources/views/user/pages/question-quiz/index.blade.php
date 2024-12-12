6176a39a04697450b5e6cbd8af0d38a091e1240d
nesaathussholeha <nesaathussholeha@gmail.com>
2024-12-12 08:52:47 +0700

    Update index.blade.php

--- resources/views/user/pages/question-quiz/index.blade.php
+++ resources/views/user/pages/question-quiz/index.blade.php
@@ -211,20 +211,32 @@
                     // Kode lainnya
                     $('#title').html(response.data.quiz.course_title);
                     $('.text-white.border-0.py-2').off('click').on('click', function() {
-                        let answer = [];
-                        for (let index = 1; index <= response.data.paginate.last_page; index++) {
-                            const storedAnswer = localStorage.getItem(`answer_${index}`);
-                            if (storedAnswer) {
-                                answer.push(storedAnswer);
-                            } else {
-                                answer.push(null);
+                        Swal.fire({
+                            title: "Konfirmasi",
+                            text: "Apakah Anda yakin ingin mengirimkan quiz? Jawaban yang telah dikirim tidak bisa diubah!",
+                            icon: "warning",
+                            showCancelButton: true,
+                            confirmButtonText: "Ya, Kirim sekarang",
+                            cancelButtonText: "Batal"
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                let answer = [];
+                                for (let index = 1; index <= response.data.paginate
+                                    .last_page; index++) {
+                                    const storedAnswer = localStorage.getItem(
+                                    `answer_${index}`);
+                                    if (storedAnswer) {
+                                        answer.push(storedAnswer);
+                                    } else {
+                                        answer.push(null);
+                                    }
+                                }
+                                submit_quiz(response.data.user_quiz.id, answer);
+                                localStorage.removeItem('current_page');
+                                removeChecked(response.data.paginate.last_page);
                             }
-                        }
-                        submit_quiz(response.data.user_quiz.id, answer);
-                        localStorage.removeItem('current_page');
-                        removeChecked(response.data.paginate.last_page);
+                        });
                     });
-
                     $('#status_question').html(
                         `<h4 class="text-white">${response.data.paginate.current_page} dari ${response.data.paginate.last_page} soal</h4>`
                     );
