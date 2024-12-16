34a060e362d75f4a20d488f7b4ad18434ea070bb
ArdianS18 <ardiansupriadi464@gmail.com>
2024-12-16 15:15:11 +0700

    update fetch untuk ranked in student

--- resources/views/student/pages/dashboard/ranks/index.blade.php
+++ resources/views/student/pages/dashboard/ranks/index.blade.php
@@ -5,13 +5,11 @@
     <div class="card p-3">
         <div class="card-body">
             <p><b>Peringkat Anda</b></p>
-            <p class="text-dark"><b><i class="fas fa-trophy me-2"></i>{{ session('user')['name'] }}, anda berada di peringkat 5</b></p>
+            <p class="text-dark"><b><i class="fas fa-trophy me-2"></i>{{ session('user')['name'] }}, anda berada di peringkat <span id="ranked"></span></b></p>
             <div class="col-6 mb-3">
                 <div class="d-flex gap-2">
-                    <input type="text" name="search" id="search" class="form-control">
-                    <select name="filter" id="filter" class="form-control" aria-placeholder="sekolah">
-                        <option value="">Sekolah</option>
-                    </select>
+                    <input type="text" name="name" id="search" class="form-control">
+                    <select name="filter" id="filter" class="form-control" aria-placeholder="sekolah"></select>
                 </div>
             </div>
             <table class="table table-hover">
@@ -27,27 +25,63 @@
 
                 </tbody>
             </table>
+            <div class="d-flex justify-content-end">
+                <nav id="pagination"></nav>
+            </div>
         </div>
     </div>
 @endsection
 
 @push('script')
 <script>
-    $(document).ready(function() {
-        function get(page) {
+
+        $(document).ready(function () {
             $.ajax({
                 type: "GET",
-                url: "{{ config('app.api_url') }}/api/student/list-range",
+                url: "{{ config('app.api_url') }}/api/schools-all",
                 headers: {
                     Authorization: 'Bearer {{ session('hummaclass-token') }}'
                 },
                 dataType: "json",
-                data: {
-                    page: page
-                },
-                success: function(response) {
-                    console.log('API Response:', response);  
+                success: function (response) {
+                    $('#filter').empty();
+                    $('#filter').append(`<option value="">Pilih Sekolah</option>`);
+                    $.each(response.data, function (index, value) { 
+                        $('#filter').append(`<option value="${value.id}">${value.name}</option>`);
+                    });
+                }
+            });
+        });
 
+        let debounceTimer;
+
+        $('#search').keyup(function() {
+            clearTimeout(debounceTimer);
+            debounceTimer = setTimeout(function() {
+                get(1);
+            }, 500);
+        });
+
+        $('#filter').change(function (e) { 
+            e.preventDefault();
+            get(1);
+        });
+
+        function get(page) {
+            $.ajax({
+                type: "GET",
+                url: "{{ config('app.api_url') }}/api/student/list-range?page=" + page,
+                headers: {
+                    Authorization: 'Bearer {{ session('hummaclass-token') }}'
+                },
+                data: {
+                    name: $('#search').val(), 
+                    school_id: $('#filter').val(), 
+                },
+                dataType: "json",
+                success: function(response) {
+                    $('#ranked').empty();
+                    $('#ranked').append(response.data.position.position);
                     if (response.success && response.data.data.length > 0) {
                         let rankListHTML = '';
                         response.data.data.forEach((student, index) => {
@@ -61,6 +95,7 @@
                             `;
                         });
                         $('#rank-list').html(rankListHTML);  
+                        $('#pagination').html(handlePaginate(response.data.paginate));
                     } else {
                         $('#rank-list').html('<tr><td colspan="4">Tidak ada data ditemukan</td></tr>');
                     }
@@ -77,6 +112,6 @@
         }
 
         get(1);
-    });
+
 </script>
 @endpush
