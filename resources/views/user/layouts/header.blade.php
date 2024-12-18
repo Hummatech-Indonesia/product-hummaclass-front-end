b5847fdfaa86e58b66f8afc52a59c67c1784ad40
nesaathussholeha <nesaathussholeha@gmail.com>
2024-12-18 14:31:24 +0700

    Update header.blade.php

--- resources/views/user/layouts/header.blade.php
+++ resources/views/user/layouts/header.blade.php
@@ -268,11 +268,12 @@
                                                 <div class="user-profile d-flex align-items-center">
                                                     <a
                                                         href="{{ route('dashboard.users.profile', session('user')['id']) }}">
-                                                        <img src="{{ session('user')['photo'] ?? asset(asset('assets/img/icons/user.svg')) }}"
+                                                        <img src="{{ session('user')['photo'] && file_exists(public_path(session('user')['photo'])) ? asset(session('user')['photo']) : asset('assets/img/icons/user.svg') }}"
                                                             class="rounded rounded-circle photo-user"
                                                             style="width: 48px; height: 48px; object-fit: cover;"
                                                             alt="User Photo">
                                                     </a>
+
                                                     <button type="submit" class="btn shadow-none py-3 ms-3">Keluar</button>
                                                 </div>
                                             </form>
